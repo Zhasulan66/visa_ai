9df3e0fab781b9f1ab381630bd18480633f8c044
Zhasulan66 <zhasulan0404@gmail.com>
2025-11-02 21:55:55 +0500

    fix swagger permission bug

--- visa_ai/settings.py
+++ visa_ai/settings.py
@@ -60,13 +60,14 @@ CORS_ALLOW_ALL_ORIGINS = True  # for testing
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = "visa_ai.urls"
