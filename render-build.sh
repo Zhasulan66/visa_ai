10d73f17ca3c87e992f0e6fbbe45eb54684f2ada
Zhasulan66 <zhasulan0404@gmail.com>
2025-11-02 21:20:33 +0500

    fix auto super user creation

--- render-build.sh
+++ render-build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py create_admin
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
