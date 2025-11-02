71544dab387e4b1833b02dee629005a516f7d06c
Zhasulan66 <zhasulan0404@gmail.com>
2025-11-02 23:43:52 +0500

    Fix swagger static files 2

--- visa_ai/settings.py
+++ visa_ai/settings.py
@@ -30,7 +30,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['visa-ai.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -57,10 +57,17 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'api.User'  # your custom user
 
 INSTALLED_APPS += ['corsheaders']
-CORS_ALLOW_ALL_ORIGINS = True  # for testing
+#CORS_ALLOW_ALL_ORIGINS = True  # for testing
+CSRF_TRUSTED_ORIGINS = ['https://visa-ai.onrender.com']
+
+# If you have CORS installed
+CORS_ALLOWED_ORIGINS = [
+    'https://visa-ai.onrender.com',
+]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -139,10 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# For production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
